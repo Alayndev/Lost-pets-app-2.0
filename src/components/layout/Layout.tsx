0f0647bdbd20211c462077ad219b306ad9106801
alayn <saezalayn@gmail.com>
2022-03-22 11:27:52 -0300

    MUI and Header

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,17 +1,12 @@
 import React from "react";
-import { SearchForm } from "../search-form/SearchForm";
 import { Outlet } from "react-router-dom";
+import { Header } from "components/header/Header";
 import css from "./layout.css";
 
 function Layout() {
   return (
     <>
-      <header className={css.header}>
-        <div className={css["header-content"]}>
-          <SearchForm></SearchForm>
-        </div>
-      </header>
-
+      <Header />
       <Outlet />
     </>
   );
