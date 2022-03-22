c50cc6fff6e989db492af4f3432950d8edd65882
alayn <saezalayn@gmail.com>
2022-03-21 22:20:27 -0300

    Initial version

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import css from "./home.css";
 
 // Solo para deploy Initial Version
 import { Link } from "react-router-dom";
 
 function Home() {
+  // Solo para deploy Initial Version
+  useEffect(() => {
+    const exist = fetch(
+      "https://lost-pet-finder-app.herokuapp.com/users/registered?email=saezalayn@gmail.com",
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    )
+      .then((res) => res.json())
+      .then((json) => console.log(json));
+  }, []);
+
+  console.log("Hola");
+
   return (
     <>
       <div className={css.container}>
