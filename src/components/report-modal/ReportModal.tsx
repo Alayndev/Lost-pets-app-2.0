590416b4fc83f3ea1250231fd437fd24677de825
alayn <saezalayn@gmail.com>
2022-03-31 18:40:45 -0300

    Home Page without logic

---
+++ src/components/report-modal/ReportModal.tsx
@@ -0,0 +1,113 @@
+import React from "react";
+import { getLocalStorageItem } from "hooks/useLocalStorage";
+import { sendReport } from "hooks/useSendReport";
+import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+import css from "./reportModal.css";
+import { PrimaryButton } from "ui/buttons";
+
+function ReportModal({ pet, children }) {
+  const navigate = useNavigate();
+  const MySwal = withReactContent(Swal);
+
+  const handleReportClick = (pet) => {
+    const token = getLocalStorageItem("token");
+    console.log(token, "token");
+
+    if (!token) {
+      navigate("/login", { replace: true });
+    } else {
+      // abrir modal al estar logeado el user
+
+      const handleReportSubmit = async (e) => {
+        e.preventDefault();
+        console.log("Hola");
+
+        // API
+        const report = {
+          petId: pet.objectID,
+          petName: pet.fullName,
+          fullName: e.target.name.value,
+          phoneNumber: e.target.tel.value,
+          report: e.target.report.value,
+        };
+        console.log(report, "report");
+
+        try {
+          const reportSent = await sendReport(report, token);
+
+          console.log(reportSent);
+
+          if (reportSent.error) {
+            Swal.fire({
+              icon: "error",
+              text: `${reportSent.error}. ${report.fullName}, agredecemos la información que intenta brindar acerca de ${report.petName}, pero la misma ya ha sido reportada.`,
+            });
+          } else {
+            Swal.fire({
+              icon: "success",
+              text: `${report.fullName}, muchas gracias por reportar información acerca de ${report.petName}. Se le envió un mail al dueñx con la información brindada, quizá sea contactado al teléfono brindado o puede contactar al dueñx vía email. Email del dueñx: ${reportSent.petAndOwner.user.email}.`,
+            });
+          }
+        } catch (error) {
+          console.error(error);
+        }
+      };
+
+      MySwal.fire({
+        title: (
+          <p className={css.title}>Reportar información sobre {pet.fullName}</p>
+        ),
+        width: "345px",
+        position: "center",
+        html: (
+          <form
+            onSubmit={handleReportSubmit}
+            className={css["report-pet__form"]}
+          >
+            <label className={css["report-pet__label"]}>
+              <span>TU NOMBRE</span>
+              <input
+                className={css["report-pet__input"]}
+                type="text"
+                name="name"
+                required
+              />
+            </label>
+            <label className={css["report-pet__label"]}>
+              <span>TU TELEFONO</span>
+              <input
+                className={css["report-pet__input"]}
+                type="phone"
+                name="tel"
+                required
+              />
+            </label>
+            <label className={css["report-pet__label"]}>
+              <span>¿DÓNDE LO VISTE?</span>
+              <textarea
+                className={css["report-pet__input"]}
+                name="report"
+                required
+              ></textarea>
+            </label>
+
+            <PrimaryButton> Enviar Reporte </PrimaryButton>
+          </form>
+        ),
+        showDenyButton: true,
+        showConfirmButton: false,
+        denyButtonText: `Cancelar`,
+      });
+    }
+  };
+  return (
+    <>
+      <div className="petContainer" onClick={() => handleReportClick(pet)}>
+        {children}
+      </div>
+    </>
+  );
+}
+export { ReportModal };
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -3,13 +3,7 @@ import css from "./home.css";
 import { LocationButton } from "components/location-button/LocationButton";
 import { PetCard } from "components/pet-card/PetCard";
 import { getPetsAround } from "hooks/usePets";
-import { useRecoilState } from "recoil";
-import { petDataState } from "hooks/useUserPets";
-import { useNavigate } from "react-router-dom";
-import { getLocalStorageItem } from "hooks/useLocalStorage";
-import Swal from "sweetalert2";
-import { sendReport } from "hooks/useSendReport";
-import withReactContent from "sweetalert2-react-content";
+import { ReportModal } from "components/report-modal/ReportModal";
 
 function Home() {
   const [showButton, setShowButton] = useState(true);
@@ -19,96 +13,6 @@ function Home() {
 
   const handleClick = () => {
     setShowButton(false);
-  };
-
-  // Componentizar ModalReport
-  const navigate = useNavigate();
-  const MySwal = withReactContent(Swal);
-
-  const handleReportClick = (pet) => {
-    const token = getLocalStorageItem("token");
-    console.log(token, "token");
-
-    if (!token) {
-      navigate("/login", { replace: true });
-    } else {
-      // abrir modal al estar logeado el user
-      console.log("Modal");
-
-      const handleReportSubmit = async (e) => {
-        e.preventDefault();
-        console.log("Hola");
-
-        // API
-        const report = {
-          petId: pet.objectID,
-          petName: pet.fullName,
-          fullName: e.target.name.value,
-          phoneNumber: e.target.tel.value,
-          report: e.target.report.value,
-        };
-        console.log(report, "report");
-
-        try {
-          const reportSent = await sendReport(report, token);
-
-          console.log(reportSent);
-
-          if (reportSent.error) {
-            Swal.fire({
-              icon: "error",
-              text: `${reportSent.error}. ${report.fullName}, agredecemos la información que intenta brindar acerca de ${report.petName}, pero la misma ya ha sido reportada.`,
-            });
-          } else {
-            Swal.fire({
-              icon: "success",
-              text: `${report.fullName}, muchas gracias por reportar información acerca de ${report.petName}. Se le envió un mail al dueñx con la información brindada, quizá sea contactado al teléfono brindado o puede contactar al dueñx vía email. Email del dueñx: ${reportSent.petAndOwner.user.email}.`,
-            });
-          }
-        } catch (error) {
-          console.error(error);
-        }
-      };
-
-      MySwal.fire({
-        title: <p>Reportar info de {pet.fullName}</p>,
-        html: (
-          <form onSubmit={handleReportSubmit}>
-            <label className="report-pet__label">
-              <span>TU NOMBRE</span>
-              <input
-                className="report-pet__input input is-large"
-                type="text"
-                name="name"
-                required
-              />
-            </label>
-            <label className="report-pet__label">
-              <span>TU TELEFONO</span>
-              <input
-                className="report-pet__input input is-large"
-                type="phone"
-                name="tel"
-                required
-              />
-            </label>
-            <label className="report-pet__label">
-              <span>¿DÓNDE LO VISTE?</span>
-              <textarea
-                className="report-pet__input textarea"
-                name="report"
-                required
-              ></textarea>
-            </label>
-
-            <button> Enviar Reporte </button>
-          </form>
-        ),
-        showDenyButton: true,
-        showConfirmButton: false,
-        denyButtonText: `Cancelar`,
-      });
-    }
   };
 
   return (
@@ -125,20 +29,17 @@ function Home() {
             <h1>Mascotas perdidas cerca tuyo: {pets.length} </h1>
             <div className={css.petsContainer}>
               {pets.map((pet) => (
-                // aca modal?
-                <div
-                  key={pet.objectID}
-                  className="petContainer"
-                  onClick={() => handleReportClick(pet)}
-                >
-                  <PetCard
-                    key={pet.objectID}
-                    picture={pet.pictureURL}
-                    name={pet.fullName}
-                    description={pet.description}
-                    loc={pet.loc}
-                    type={"Reportar"}
-                  />
+                <div key={pet.objectID}>
+                  <ReportModal pet={pet}>
+                    <PetCard
+                      key={pet.objectID}
+                      picture={pet.pictureURL}
+                      name={pet.fullName}
+                      description={pet.description}
+                      loc={pet.loc}
+                      type={"Reportar"}
+                    />
+                  </ReportModal>
                 </div>
               ))}
             </div>
